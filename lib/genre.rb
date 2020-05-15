952bc666a0822a692ff6193d7b2c38493c740879
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-15 01:23:44 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,20 +1,3 @@
 class Genre
-  attr_accessor :name
-  @@all = []
-  def initialize(name)
-    @name = name
-    @@all << @genre
-  end
-  
-  def self.all
-    @@all
-  end
-  
-  def songs
-    Song.all.select {|song| song.genre == self}
-  end
-  
-  def artists
-    self.songs.collect {|song| song.artist}
-  end
+
 end
