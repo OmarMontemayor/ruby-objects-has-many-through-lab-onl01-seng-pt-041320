2a85a0342f976f8b00684785b227a5f78b508295
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-15 01:38:34 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,7 +8,22 @@ class Artist
     @name = name
     @@all << self
   end
-    
+  
+  def new_song(name, genre)
+    Song.new(name, self, genre)
+    Song.artist = self
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres
+    genres =[]
+    self.songs.each {|song| genres << song.genre}
+    genres
+    binding.pry
+  end
   def self.all
     @@all
   end
