5388697466bb26a7e646884ed0cdad1dbde12aab
Omar Montemayor <omarmontemayor11@yahoo.com>
2020-05-15 02:04:34 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,7 +8,18 @@ class Doctor
   end
   
   def new_appointment(date, patient)
-    Appointment.new()
+    Appointment.new(date, patient, self)
+  end
+
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def patients
+    patients = []
+    self.appointments.each {|appointment| patients << appointment.patient}
+    patients
+  end
   
   def self.all
     @@all
